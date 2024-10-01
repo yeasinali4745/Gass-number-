7997fdb537175aa74c78d6eeb51bf9eeeb4e7423
yeasinali4745 <yeasinali4745@gmail.com>
2024-10-01 18:20:55 +0600

    Add files via upload

---
+++ Gass Number.js
@@ -0,0 +1,20 @@
+let win=0,los=0;
+const label =parseInt(prompt('Enter Game label:')); 
+for (var index = 1; index <=label; index++) {
+  var gassnumber=parseInt(prompt('Enter your Gass number(0-4)'));
+  var ganaratNumber=Math.floor(Math.random()*4);
+  if(gassnumber===ganaratNumber){
+     win++;
+     alert("You win");
+     
+  }
+  else {
+     alert("You los");
+     alert("Correct number was" + ganaratNumber);
+     los++;
+  }
+}
+document.write("You win total=" + win +"<br/>" +"<br/>");
+
+document.write("You los total=" + los +"<br/>");
+document.getElementById('Y').innerHTML='hi';
